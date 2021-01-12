0aba2e93df50b9df1b53631da7e631c13b609a18
Daniel <55975773+DanielBoyko@users.noreply.github.com>
2021-01-12 19:06:59 +0200

    Small syntax fix

--- src/LabWork1/StringManager.java
+++ src/LabWork1/StringManager.java
@@ -3,7 +3,7 @@ package LabWork1;
 public class StringManager {
 
     /* 19. Створіть метод, який перевертає будь яке слово. Наприклад - на вході травень на виході ьневарт */
-    public String Reverse(String str){
+    public String Reverse(String str) {
         return new StringBuilder(str).reverse().toString();
     }
 }
--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,8 @@
 import LabWork1.ArrayManager;
+import LabWork1.StringManager;
 
 public class Program {
     public static void main(String[] args) {
-        
+
     }
 }
