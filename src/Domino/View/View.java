702098cf62ad187e0a486fd78f680fb28f0cebb4
Daniel Boyko <55975773+navigati0n@users.noreply.github.com>
2021-02-02 21:46:23 +0200

    Domino

---
+++ src/Domino/View/View.java
@@ -0,0 +1,23 @@
+package Domino.View;
+
+
+import Domino.Model.Domino;
+
+import java.util.Collection;
+
+public class View {
+    public void menuInterface(int marketSize) {
+        System.out.println("1. Add dominoes to the beginning of the board");
+        System.out.println("2. Add dominoes to the end of the board");
+        System.out.println("3. Take dominoes from the market (In stock " + marketSize + ")");
+        System.out.println("4. Surrender");
+    }
+
+    public void printInfo(String message, Collection<Domino> collection) {
+        System.out.print(message);
+        for (Domino domino : collection) {
+            System.out.print(domino);
+        }
+        System.out.println();
+    }
+}
